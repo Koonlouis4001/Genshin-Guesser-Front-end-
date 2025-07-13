fc6ae784279f1b8e91ba9a75924b90c7cc1cdbd4
Koonlouis <Koonlouis4001@hotmail.com>
2025-07-13 20:28:45 +0700

    add new button for answing after select the character to prevent misclick answer

--- app/component/GenshinGuesserSelector.tsx
+++ app/component/GenshinGuesserSelector.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -6,70 +7,75 @@ type GenshinGuesserSelectorProps = {
   guessCharacterList: any[];
 };
 
-export function GenshinGuesserSelector({ guessCharacter,guessCharacterList }: GenshinGuesserSelectorProps) {
+export function GenshinGuesserSelector({
+  guessCharacter,
+  guessCharacterList,
+}: GenshinGuesserSelectorProps) {
   const [characters, setCharacters] = useState<any[]>([]);
+  const [currentSelectCharacter, setCurrentSelectCharacter] = useState<any>();
   const [filterCharacters, setFilterCharacters] = useState<any[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
   const [value, setValue] = useState("");
 
   useEffect(() => {
     async function fetchCharacters() {
-      setLoading(true);
-      setError(null);
-      try {
-        const response = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/genshin-characters`
-        );
-        setCharacters(response.data);
-      } catch {
-        setError("Failed to fetch characters");
-      } finally {
-        setLoading(false);
-      }
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/genshin-characters`
+      );
+      setCharacters(response.data);
     }
     fetchCharacters();
   }, []);
 
   useEffect(() => {
-    if(!guessCharacterList || guessCharacterList.length == 0) {
-      console.log("guessCharacterList : " + guessCharacterList)
+    if (!guessCharacterList || guessCharacterList.length == 0) {
+      console.log("guessCharacterList : " + guessCharacterList);
       setFilterCharacters(characters);
       return;
     }
-    const tempFilteredCharacters = characters.filter(character => 
-      !guessCharacterList.some(guessed => guessed.id === character.id)
+    const tempFilteredCharacters = characters.filter(
+      (character) =>
+        !guessCharacterList.some((guessed) => guessed.id === character.id)
     );
     console.log("Filtered characters: ", tempFilteredCharacters);
     setFilterCharacters(tempFilteredCharacters);
-  }, [characters,guessCharacterList]);
+  }, [characters, guessCharacterList]);
 
-  const selectCharacter = (id: any) => {
-    if(!id) {
-        return;
+  const selectCharacter = () => {
+    if (!currentSelectCharacter) {
+      console.log("No character selected");
+      return;
     }
     setValue("");
-    guessCharacter(characters.find(character => character.id == id));
-  }
+    guessCharacter(currentSelectCharacter);
+    setCurrentSelectCharacter(null);
+  };
 
   return (
-    <select 
-    className="custom-selector"
-    value={value}
-    onChange={(e) => {
-        setValue(e.target.value);
-        selectCharacter(e.target.value);
-        }}>
-        <option value="">
-            Please guess a character
-        </option>
-        {characters.length != 0 && 
-        filterCharacters.map((character) => (
-          <option key={character.id} value={character.id} className="text-black-700 dark:text-black-300">
-            {character.name}
-            {/*  - {character.region} - {character.vision} - {character.weapon} - {character.affiliation} - {character.version} */}
-          </option>
-        ))}
-    </select>
+    <div className="selector-board">
+      <select
+        className="custom-selector"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+          setCurrentSelectCharacter(characters.find((character) => character.id == e.target.value));
+        }}
+      >
+        <option value="">Please guess a character</option>
+        {characters.length != 0 &&
+          filterCharacters.map((character) => (
+            <option
+              key={character.id}
+              value={character.id}
+              className="text-black-700 dark:text-black-300"
+            >
+              {character.name}
+              {/*  - {character.region} - {character.vision} - {character.weapon} - {character.affiliation} - {character.version} */}
+            </option>
+          ))}
+      </select>
+      <Button variant="contained" className="guess-button" onClick={selectCharacter}>
+        Guess
+      </Button>
+    </div>
   );
 }
